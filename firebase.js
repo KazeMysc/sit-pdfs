35db8db445383611cf66c88ae8aaa0e5feb881af
Christian Silva <kazemysc@gmail.com>
2025-06-10 23:36:17 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
-  // API e DADOS - FireBase
-  // Foram omitidos por questões de segurança
+  /* API e DADOS - FireBase
+  *⚠️ A firebaseConfig é pública por padrão. O acesso ao banco de dados é controlado via regras do Firebase. 
+  *Nenhum dado sensível está exposto.*/
   const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyA8ELDDVU9ykMuUaJVy5cZHsfa4ooq-Fbs",
+  authDomain: "sit-remote-data.firebaseapp.com",
+  databaseURL: "https://sit-remote-data-default-rtdb.firebaseio.com",
+  projectId: "sit-remote-data",
+  storageBucket: "sit-remote-data.firebasestorage.app",
+  messagingSenderId: "32377279579",
+  appId: "1:32377279579:web:7c13d6898e3abb33b2db7e",
+  measurementId: "G-E2EDQ26QQ7"
 };
 
   const app = initializeApp(firebaseConfig);
   const database = getDatabase(app);
   const textoRef = ref(database, 'msgAlert');
-
+  /*Atualiza o texto da caixa de alerta da página inicial
+  de acordo com as mudanças na propriedade msgAlert do firebase*/
   onValue(textoRef, (snapshot) => {
     document.getElementById("textAlert").innerText = snapshot.val();
   });
